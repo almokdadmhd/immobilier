c17c7fb59c15d3cd6459efb45a918e4622a2fe20
zakria <almokdad.mhd@gmail.com>
2021-05-27 09:41:48 +0200

    immo7

--- classes/controller/CreationConnexion.php
+++ classes/controller/CreationConnexion.php
@@ -15,40 +15,35 @@
     require_once "../view/ViewUser.php";
     require_once "../view/ViewTemplate.php";
     require_once "../model/ModelUser.php";
-    ViewUser::Connexion();
+    require_once "../utils/Utils.php";
+    ViewTemplate::menu();
 
-    if (isset($_POST['ajout'])) {
-        if (ModelUser::mailExistant($_POST["mail"]) && ($_POST["pass"])) {
-            ViewTemplate::alert("la connexion est correcte ", " success  ", "CreationConnexion.php");
-        }
-        if (isset($_POST['ajout'])) {
-            ModelUser::confirmMail($_POST['mail'], $_POST['pass']);
-            ViewTemplate::alert("La connexion est correcte ", "success", "CreationConnexion.php");
+    if (isset($_POST['mail'])) {
+        if (ModelUser::mailExistant($_POST["mail"])) {
+            if (ModelUser::confirmMail($_POST["mail"], (ModelUser::mailExistant($_POST["mail"])["token"]))) {
+                $mdp1 = $_POST["pass"];
+                $mdp2 = ModelUser::mailExistant($_POST["mail"])["pass"];
+                if (Utils::verification($mdp1, $mdp2)) {
+                    $_SESSION["id"] = ModelUser::mailExistant($_POST["mail"])["id"];
+                    $_SESSION["nom"] = ModelUser::mailExistant($_POST["mail"])["nom"];
+                    $_SESSION["mail"] = ($_POST["mail"]);
+                    ViewUser::Connexion();
+                } else {
+                    ViewTemplate::alert("mail est existe  ", " danger  ", "CreationConnexion.php");
+                }
+            } else {
+                ViewTemplate::alert("la connexion est pas bon  ", " danger  ", "CreationConnexion.php");
+            }
         } else {
+            ViewTemplate::alert("mail est n'existe pas  ", " danger  ", "CreationConnexion.php");
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
+    ViewUser::Connexion();
 
 
     ViewTemplate::footer();
 
-    if (isset($_POST['deconnexion'])) {
-        session_unset();
-        session_destroy();
-        header('location:Accueil.php');
-    } else {
-    }
+
     ?>
     <script src="../../js/jquery-3.5.1.min.js"></script>
     <script src="../../js/all.min.js"></script>
--- classes/model/ModelUser.php
+++ classes/model/ModelUser.php
@@ -22,5 +22,187 @@ class ModelUser
         $requt = $data->prepare("UPDATE user set confirme ='1', actif='1'  WHERE mail =:mail AND token=:token");
         $requt->execute([':mail' => $mail, ':token' => $token]);
     }
-    
+
+
+    private $id;
+    private $nom;
+    private $prenom;
+    private $mail;
+    private $pass;
+    private $tel;
+    private $role;
+    private $confirme;
+    private $actif;
+    private $token;
+    private $annonces = [];
+    private $favoris = [];
+
+    public function __construct($id)
+    {
+        $donneeUser = $this->getById($id);
+        $this->id = $id;
+        $this->nom = $donneeUser['nom'];
+        $this->prenom = $donneeUser['prenom'];
+        $this->mail = $donneeUser['mail'];
+        $this->pass = $donneeUser['pass'];
+        $this->tel = $donneeUser['tel'];
+        $this->role = $donneeUser['role'];
+        $this->confirme = $donneeUser['confirme'];
+        $this->actif = $donneeUser['actif'];
+        $this->token = $donneeUser['token'];
+        $this->annonces = [2];
+        $this->favoris = [1];
+    }
+
+
+    // ajoute utilisateur via form CREATE
+    public static function addUsere($nom, $prenom, $mail, $tel, $mdp, $code)
+    {
+        $datay = connexion();
+        $rPrep = $datay->prepare("INSERT INTO user (nom,prenom,mail,pass,tel,token) VALUES (:nom,:prenom,:mail,:mdp,:tel,:code)");
+        $rPrep->execute([':nom' => $nom, ':prenom' => $prenom, ':mail' => $mail, ':tel' => $tel, ':mdp' => $mdp, ':code' => $code]);
+    }
+
+    //recup user via mail
+
+    public static function getByMail($mail)
+    {
+        $datay = connexion();
+        $rPrep = $datay->prepare("SELECT * FROM user WHERE mail=?");
+        $rPrep->execute([$mail]);
+        return $rPrep->fetch(pdo::FETCH_ASSOC);
+    }
+
+    //recup user via id
+
+    public static function getById($id)
+    {
+        $datay = connexion();
+        $rPrep = $datay->prepare("SELECT * FROM user WHERE id=?");
+        $rPrep->execute([$id]);
+        return $rPrep->fetch(pdo::FETCH_ASSOC);
+    }
+
+    //confirme compte via token
+    public static function confirmCompte($mail)
+    {
+        $datay = connexion();
+        $rPrep = $datay->prepare("UPDATE user SET confirme='1' WHERE mail=? ");
+        $rPrep->execute([$mail]);
+    }
+    //modif Colonne
+    public static function modifColonneUser($colonne, $valeur, $mail)
+    {
+        $datay = connexion();
+        $rPrep = $datay->prepare("UPDATE user SET $colonne=? WHERE mail=? ");
+        $rPrep->execute([$valeur, $mail]);
+    }
+
+    //GETTER user
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getMail()
+    {
+        return $this->mail;
+    }
+    public function getPass()
+    {
+        return $this->pass;
+    }
+    public function getTel()
+    {
+        return $this->tel;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+    public function getConfirme()
+    {
+        return $this->confirme;
+    }
+    public function getActif()
+    {
+        return $this->actif;
+    }
+    public function getToken()
+    {
+        return $this->token;
+    }
+    public function getAnnonces()
+    {
+        return $this->annonces;
+    }
+    public function getFavoris()
+    {
+        return $this->favoris;
+    }
+
+
+    //SETTER User
+    public function setNom($newNom)
+    {
+        $this->nom = $newNom;
+        return $this;
+    }
+    public function setPrenom($newPrenom)
+    {
+        $this->prenom = $newPrenom;
+        return $this;
+    }
+    public function setMail($newMail)
+    {
+        $this->mail = $newMail;
+        return $this;
+    }
+    public function setPass($newPass)
+    {
+        $this->pass = $newPass;
+        return $this;
+    }
+    public function setTel($newTel)
+    {
+        $this->tel = $newTel;
+        return $this;
+    }
+    public function setRole($newRole)
+    {
+        $this->role = $newRole;
+        return $this;
+    }
+    public function setConfirme($newConfirme)
+    {
+        $this->confirme = $newConfirme;
+        return $this;
+    }
+    public function setActif($newActif)
+    {
+        $this->actif = $newActif;
+        return $this;
+    }
+    public function setToken($newToken)
+    {
+        $this->token = $newToken;
+        return $this;
+    }
+    public function setAnnonces($newAnnonces)
+    {
+        $this->annonces = $newAnnonces;
+        return $this;
+    }
+    public function setFavoris($newFavoris)
+    {
+        $this->favoris = $newFavoris;
+        return $this;
+    }
 }
